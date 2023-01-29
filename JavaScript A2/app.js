98c956da04dcb6d4c1696f2017627f85eaaf5bef
M. Mudassir Awan <121383917+Mudasssirr@users.noreply.github.com>
2023-01-29 09:38:13 +0500

    added JavaScript assignment 2 file

---
+++ JavaScript A2/app.js
@@ -0,0 +1,42 @@
+// Chapter 2
+
+//Question 1
+//var username;
+
+
+//Question 2
+//var myName = "Muhammad Mudassir Awan";
+
+
+//Question 3
+//var message;
+//message = "Hello World";
+//alert (message)
+
+//Question 4
+//alert (myName);
+
+//var age = "19 years old";
+//alert (age);
+
+//var proffession = "Certified Mobile App Developer";
+//alert (proffession);
+
+//Question 5
+//var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+//alert (pizza);
+
+// Question 6
+//var email = "My email address is muhammadmusassir.090@gmail.com";
+//alert (email);
+
+//Question 7
+//var book = "I am trying to learn from the book A smarter way to learn JavaScript";
+//alert (book);
+
+//Question 8
+//document.write("Yah! I can write HTML content through JavaScript");
+
+//Question 9
+//var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert (design);
